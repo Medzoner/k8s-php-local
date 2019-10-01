416ad1495d55d318ab1a36ef510caff81770c71c
medzoner <medzoner@medzoner.com>
2019-10-01 23:50:39 +0200

    wording

--- helm_upgrade.sh
+++ helm_upgrade.sh
@@ -2,7 +2,7 @@
 
 HELM_CMD="microk8s.helm"
 HELM_CMD_OPTION="upgrade"
-HELM_CMD_OPTION="install --name"
+#HELM_CMD_OPTION="install --name"
 
 $(echo ${HELM_CMD}) $(echo ${HELM_CMD_OPTION}) global --set env=local -f ./charts/global/releases/local/values.yaml ./charts/global
 $(echo ${HELM_CMD}) $(echo ${HELM_CMD_OPTION}) php    --set env=local -f ./charts/php/releases/local/values.yaml ./charts/php
