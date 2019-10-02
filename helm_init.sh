b034ce7e126146547278db8b57c1c7e88e8f7cb7
MEdzoner <pipeline@leadersleague.com>
2019-10-02 11:47:34 +0200

    fix helm init

--- helm_init.sh
+++ helm_init.sh
@@ -2,10 +2,10 @@
 
 microk8s.enable helm
 
-#microk8s.kubectl config view --raw > config.yml
-
-microk8s.kubectl create serviceaccount --namespace kube-system tiller
 microk8s.kubectl create serviceaccount --namespace kube-system tiller
 microk8s.kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
-microk8s.helm init --upgrade --service-account tiller --override spec.selector.matchLabels.'name'='tiller',spec.selector.matchLabels.'app'='helm' --output yaml | sed 's@apiVersion: extensions/v1beta1@apiVersion: apps/v1@' | kubectl apply -f -
+
+#microk8s.kubectl config view --raw > config.yml
+#export KUBECONFIG=$(pwd)/config.yml
+helm init --upgrade --service-account tiller --override spec.selector.matchLabels.'name'='tiller',spec.selector.matchLabels.'app'='helm' --output yaml | sed 's@apiVersion: extensions/v1beta1@apiVersion: apps/v1@' | kubectl apply -f -
 microk8s.kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
--- microk8s_init.sh
+++ microk8s_init.sh
@@ -3,3 +3,4 @@ sudo snap install microk8s --classic
 sudo usermod -a -G microk8s $(echo $UID)
 
 # restart user session
+microk8s.enable dns
